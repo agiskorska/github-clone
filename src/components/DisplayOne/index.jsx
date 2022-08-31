471550ef311f4bf5c632f5d13a2338f836cbf40c
agiskorska <skorskaga@gmail.com>
2022-08-31 12:25:51 +0100

    Add some styles to DisplayOne component

--- src/components/DisplayOne/index.jsx
+++ src/components/DisplayOne/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import './index.css'
+
 function index({data}) {
   console.log(data)
   return (
     <div className='root'>
-      <a href={data["html_url"]}>{data.name}</a><span className='pill'>{data.visibility}</span>
+      <a className='title' href={data["html_url"]}>{data.name}</a><span className='pill'>{data.visibility}</span>
       <p>{data.description ? data.description : data.name +' by ' +data.owner.login}</p>
       
     </div>
