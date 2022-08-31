25650ff4578ef73433b390aa614bc207c29ea6a4
agiskorska <skorskaga@gmail.com>
2022-08-31 14:28:39 +0100

    Add more styles

--- src/components/DisplayOne/index.jsx
+++ src/components/DisplayOne/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCodeFork, faStar } from '@fortawesome/free-solid-svg-icons'
 
 import './index.css'
 
@@ -8,7 +10,15 @@ function index({data}) {
     <div className='root'>
       <a className='title' href={data["html_url"]}>{data.name}</a><span className='pill'>{data.visibility}</span>
       <p>{data.description ? data.description : data.name +' by ' +data.owner.login}</p>
-      
+      <div className='icons-wrapper'>
+        <FontAwesomeIcon icon={faCodeFork} />
+        <div>{data.forks}</div>
+      </div>
+      <div className='icons-wrapper'>
+        <FontAwesomeIcon icon={faStar} />
+        <div>{data.stargazers_count}</div>
+      </div>
+      <div className='icons-wrapper'>Updated on {data.updated_at}</div>      
     </div>
   )
 }
